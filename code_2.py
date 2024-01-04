29a5a56e30638dff2fa19bf8fc3577fe676591ec
semenk69 <152292047+semenk69@users.noreply.github.com>
2024-01-05 01:04:36 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,40 @@
+if __name__ == "__main__":
+    return data
+
+    for item in data:
+def generate_random_data():
+
+def main():
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+    main()
+
+import random
+if __name__ == "__main__":
+
+import random
+    main()
+        print(f"Random Number: {item}")
+def main():
+def generate_random_data():
+
+    return data
+    for item in data:
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    return data
+    data = generate_random_data()
+    for item in data:
+
+def generate_random_data():
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
